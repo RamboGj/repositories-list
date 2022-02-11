4e7cb3ac7e7943396587dd9485130c251cb3ae66
João Rambo <jpramboguanabara@hotmail.com>
2022-02-11 11:16:16 -0300

    filter repo name by its name input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './styles.css'
 
-function App() {
+export function App() {
+  const [search, setSearch] = useState<string>('')
+  const [info, setInfo] = useState<any>([])
+
+  const lowerSearch = search.toLowerCase()
+
+  useEffect(() => {
+    const baseURl = `https://api.github.com/users/RamboGj`
+    const url = `${baseURl}/repos`
+    fetch(url)
+    .then(response => response.json())
+    .then(response => {
+      console.log('response :', response)
+      setInfo(response)
+      console.log('infos :',info)
+    })
+  }, [])
+
+  const filteredChapters = info.filter((inf: any) => {
+    return inf.name.toLowerCase().includes(lowerSearch)
+  })
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <body>
+      <div className="text-input">
+        <label htmlFor="searchbox">Search</label>
+        <input 
+        className= "searchbox"
+        id='searchbox'
+        value={search}
+        type="search" 
+        onChange={(e) => setSearch(e.target.value)}
+        />
+        <span className="separator"> </span>
+      </div>
+      <div>
+        <ul className="repoList">
+          {filteredChapters.map((repoData: any) => {
+            return (
+              <li>
+                <a href={`https://github.com/RamboGj/${repoData.name}`} >
+                  {repoData.full_name}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </body>
+  )
 }
-
-export default App;
